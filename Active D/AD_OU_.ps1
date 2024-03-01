b908a852abc37fe5fd0c49ae694d014c785cd2e4
cdr3xl3r <126210495+cdr3xl3r@users.noreply.github.com>
2024-02-29 20:56:53 -0500

    Update AD_OU_.ps1

--- Active D/AD_OU_.ps1
+++ Active D/AD_OU_.ps1
@@ -3,7 +3,7 @@ foreach ($ou in $ous) {
     # Check if the OU already exists
     $ouExists = Get-ADOrganizationalUnit -Filter "Name -eq '$ou'" -ErrorAction SilentlyContinue
     if (-not $ouExists) {
-        New-ADOrganizationalUnit -Name $ou -Path "DC=dc,DC=drexle735,DC=local"
+        New-ADOrganizationalUnit -Name $ou -Path "DC=drexle735,DC=local"
         Write-Host "Created OU: $ou"
     } else {
         Write-Host "OU $ou already exists."
